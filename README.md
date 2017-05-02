78772c60210f338a3af301d723528ac8087cf859
Jeff Andersen <jeffandersen@gmail.com>
2017-05-01 21:10:06 -0300

    Implement SSO, Update README to show how to use Grafton

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# node-restify-sample-provider
-A minimal provider using Restify
+# Sample Manifold Provider (Node.js/Restify)
+
+This repo contains a a minimal provider using Restify and Node.js.
+It provides digital cat bonnets as a service; fun!
+
+# Testing the app with Grafton
+
+Grafton is the test framework used to verify your provider implementation is
+correct.
+
+Contact [support@manifold.co](mailto:support@manifold.co) to get access to
+Grafton.
+
+
+To use Grafton to verify this sample provider:
+
+```bash
+# create a test master key for grafton to use when acting as Manifold
+# this file is written as masterkey.json
+grafton generate
+
+# Set environment variables to configure the test app
+# MASTER_KEY is the public_key portion of masterkey.json
+export MASTER_KEY="TtziSVE/9lnZ7fRYhWtGZpuXUKJ82FunK9rM0IkuP/0"
+# CONNECTOR_URL is the url that Grafton will listen on. It corresponds to
+# Grafton's --sso-port flag.
+export CONNECTOR_URL=http://localhost:3001/v1
+
+# Set fake OAuth 2.0 credentials. The format of these are specific, so you can
+# reuse the values here.
+export CLIENT_ID=21jtaatqj8y5t0kctb2ejr6jev5w8
+export CLIENT_SECRET=3yTKSiJ6f5V5Bq-kWF0hmdrEUep3m3HKPTcPX7CdBZw
+
+# install dependencies and run the sample app
+npm install
+npm start
+
+# In another shell, run grafton.
+grafton test --product=bonnets --plan=small --region=aws::us-east-1 \
+    --client-id=21jtaatqj8y5t0kctb2ejr6jev5w8 \
+    --client-secret=3yTKSiJ6f5V5Bq-kWF0hmdrEUep3m3HKPTcPX7CdBZw \
+    --connector-port=3001 \
+    --new-plan=large \
+    http://localhost:4567
+
+# If everything went well, you'll be greeted with plenty of green check marks!
+```
--- index.js
+++ index.js
@@ -1,28 +1,17 @@
 "use strict";
 
 var _ = require("lodash");
+var request = require("request");
 var restify = require("restify");
-var plugins = require('restify-plugins');
+var plugins = require("restify-plugins");
+var session = require("restify-memory-session");
+var auth = require('basic-auth');
 var manifoldSignature = require("@manifoldco/signature");
 
 var MASTER_KEY = process.env.MASTER_KEY ||  manifoldSignature.MASTER_KEY;
-
-var server = restify.createServer();
-
-server.use(plugins.bodyParser({
-  mapParams: true
-}));
-server.use(plugins.queryParser());
-
-server.use(function(req, res, next) {
-  var verifier = new manifoldSignature.Verifier(MASTER_KEY);
-  verifier.test(req).then(function() {
-    next();
-  }).catch(function(err) {
-    res.statusCode = err.statusCode || 500;
-    return res.json({ message: err.message });
-  });
-});
+var CONNECTOR_URL = process.env.CONNECTOR_URL;
+var CLIENT_SECRET = process.env.CLIENT_SECRET;
+var CLIENT_ID = process.env.CLIENT_ID;
 
 var data = {
   products: ["bonnets"],
@@ -35,7 +24,71 @@ var db = {
   credentials: {},
 };
 
-server.put("/v1/resources/:id", function(req, res, next) {
+var verifyMiddleware = function(req, res, next) {
+  var verifier = new manifoldSignature.Verifier(MASTER_KEY);
+  verifier.test(req).then(function() {
+    next();
+  }).catch(function(err) {
+    res.statusCode = err.statusCode || 500;
+    return res.json({ message: err.message });
+  });
+};
+
+var server = restify.createServer();
+
+server.use(plugins.bodyParser({
+  mapParams: true
+}));
+server.use(plugins.queryParser());
+server.use(session().sessionManager);
+
+server.get("/dashboard", function(req, res, next) {
+  if (!req.session.token) {
+    res.statusCode = 401;
+    return res.send("you must be logged in with Manifold");
+  }
+
+  request.get({
+    url: CONNECTOR_URL + "/users/self",
+    headers: {
+      authorization: "Bearer " + req.session.token,
+    }
+  }, function(err, resp) {
+    var html = "";
+    res.end(html);
+  });
+});
+
+server.get('/test', function(req, res, next) {
+  res.redirect('/dashboard', next);
+});
+
+// NOTE: Verifier middleware is not used for SSO requests
+server.get("/v1/sso", function(req, res, next) {
+  request.post({
+    url: CONNECTOR_URL + "/v1/oauth/tokens",
+    auth: {
+      user: CLIENT_ID,
+      pass: CLIENT_SECRET,
+    },
+    form: {
+      grant_type: "authorization_code",
+      code: req.query.code,
+    },
+    json: true,
+  }, function(err, resp) {
+    if (err || (resp.body && resp.body.error)) {
+      res.statusCode = 401;
+      return res.send("This is a page that the user would see");
+    }
+    req.session.token = resp.body.access_token;
+    req.session.resource = req.query.resource_id;
+    res.redirect(302, '/dashboard', next);
+  });
+});
+
+// Update resource
+server.put("/v1/resources/:id", verifyMiddleware, function(req, res, next) {
   if (data.plans.indexOf(req.body.plan) === -1) {
     res.statusCode = 400;
     return res.json({ message: "bad plan" });
@@ -66,7 +119,7 @@ server.put("/v1/resources/:id", function(req, res, next) {
 });
 
 // Update resource
-server.patch("/v1/resources/:id", function(req, res, next) {
+server.patch("/v1/resources/:id", verifyMiddleware, function(req, res, next) {
   if (data.plans.indexOf(req.body.plan) === -1) {
     res.statusCode = 400;
     return res.json({ message: "bad plan" });
@@ -83,7 +136,7 @@ server.patch("/v1/resources/:id", function(req, res, next) {
 });
 
 // Delete resource
-server.del("/v1/resources/:id", function(req, res, next) {
+server.del("/v1/resources/:id", verifyMiddleware, function(req, res, next) {
   var resource = db.resources[req.params.id];
   if (!resource) {
     res.statusCode = 404;
@@ -96,7 +149,7 @@ server.del("/v1/resources/:id", function(req, res, next) {
 });
 
 // Create credential
-server.put("/v1/credentials/:id", function(req, res, next) {
+server.put("/v1/credentials/:id", verifyMiddleware, function(req, res, next) {
   var resource = db.resources[req.body.resource_id];
   if (!resource) {
     res.statusCode = 404;
@@ -114,7 +167,7 @@ server.put("/v1/credentials/:id", function(req, res, next) {
 });
 
 // Delete credential
-server.del("/v1/credentials/:id", function(req, res, next) {
+server.del("/v1/credentials/:id", verifyMiddleware, function(req, res, next) {
   var credential = db.credentials[req.params.id];
   if (!credential) {
     res.statusCode = 404;
